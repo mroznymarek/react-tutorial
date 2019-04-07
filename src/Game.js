4970eb75b729bf6eeb62b9c54ed11ac54a25687e
Marek Mróz <marekmroz@wp.pl>
2019-04-08 01:14:30 +0200

    calculate winner

--- src/Game.js
+++ src/Game.js
@@ -1,16 +1,14 @@
 import React from 'react';
 
-class Square extends React.Component {
-    render() {
-        return (
-            <button 
-                className="square"
-                onClick={() => this.props.onClick}
-                >
-                {this.props.value}
-            </button>
-        );
-    }
+function Square(props) {
+    return (
+        <button
+            className="square"
+            onClick={props.onClick}
+        >
+            {props.value}
+        </button>
+    )
 }
 
 class Board extends React.Component {
@@ -18,13 +16,17 @@ class Board extends React.Component {
         super(props)
         this.state = {
             squares: Array(9).fill(null),
+            xIsNext: true,
         }
     }
 
     handleClick(i){
         const squares = this.state.squares.slice()
-        squares[i] = 'X'
-        this.setState({squares: squares})
+        squares[i] = this.state.xIsNext ? 'X' : 'O'
+        this.setState({
+            squares: squares,
+            xIsNext: !this.state.xIsNext,
+        })
     }
 
 
@@ -36,9 +38,9 @@ class Board extends React.Component {
             />
         )
     }
-    
+
     render() {
-        const status = 'Next player: X';
+        const status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O')
 
         return (
             <div>
@@ -78,5 +80,26 @@ class Game extends React.Component {
         );
     }
 }
+
+function calculateWinner(squares){
+    const lines = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6],
+    ]
+    for (let i = 0; i < lines.length; i++){
+        const [a, b, c] = lines[i]
+        if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]){
+            return squares[a]
+        }
+    }
+    return null
+}
+
 
 export default Game 
